68f2e9a49c8d9865744ef82a00b8df2cdc6b2f09
Agape <theaman249@gmail.com>
2024-11-14 01:38:43 +0200

    🚀 fixed routing issue on classes_objects_uml

--- frontend/master-oop/src/app/classes-offline/classes-offline.component.ts
+++ frontend/master-oop/src/app/classes-offline/classes-offline.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-classes-offline',
@@ -9,4 +10,9 @@ import { Component } from '@angular/core';
 })
 export class ClassesOfflineComponent {
 
+  constructor(private router:Router){}
+
+  goHome(){
+    this.router.navigate(['./home']);
+  }
 }
